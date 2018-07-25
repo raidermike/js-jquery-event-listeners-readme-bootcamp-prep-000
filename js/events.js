9d8d538ca4a9be8cb5d69306946c1d0b29f32af8
mjcabrera@gmail.com <mjcabrera@gmail.com>
2018-07-25 19:41:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,3 +17,24 @@ function frameIt() {
     $('img').addClass('tasty')
   })
 }
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+$(document).ready(function(){
+
+// call functions here
+
+});
